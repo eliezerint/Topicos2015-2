8fcb87c6c601dc2f69d1b476b50194a1b11556dd
Eliezer Macedo Da Costa <eliezermacedo90@gmail.com>
2015-08-03 21:10:49 -0300

    commit de alteração

---
+++ core/src/br/grupointegrado/SpaceInvaders/TelaBase.java
@@ -0,0 +1,21 @@
+package br.grupointegrado.SpaceInvaders;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by eli on 03/08/2015.
+ */
+public abstract class TelaBase implements Screen {
+
+    protected  MainGame game;
+
+    public TelaBase (MainGame game){
+        this.game = game;
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+}
---
+++ core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
@@ -0,0 +1,124 @@
+package br.grupointegrado.SpaceInvaders;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+
+
+
+/**
+ * Created by eli on 03/08/2015.
+ */
+public class TelaJogo extends TelaBase {
+
+    private OrthographicCamera camera;
+    private SpriteBatch  batch;
+    private Stage palco;
+    private BitmapFont fonte;
+    private Label lbPontuacao;
+
+    /**
+     * Contrutor padrão da tela de jogo
+
+     * @param game Referência para classe principal
+     */
+    public TelaJogo (MainGame game){
+        super(game);
+
+    }
+
+    /**
+     * chamada quando a tela é exibida
+     */
+
+    @Override
+    public void show() {
+      camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight( ));
+      batch = new SpriteBatch();
+      palco = new Stage(new FillViewport(camera.viewportWidth, camera.viewportHeight, camera));
+
+        initFonte();
+        initInformacoes();
+    }
+
+    private void initInformacoes(){
+        Label.LabelStyle lbEstilo  = new  Label.LabelStyle();
+        lbEstilo.font = fonte;
+        lbEstilo.fontColor = Color.WHITE;
+
+        lbPontuacao = new Label("0 pontos", lbEstilo);
+        palco.addActor(lbPontuacao);
+
+
+    }
+
+    private  void initFonte(){
+      fonte = new BitmapFont();
+    }
+
+
+
+    /**
+     * Chamado a todo quadro de atualização de Jogo (FPS)
+     * @param delta Tempo entre um quadro e outro (em segundo)
+     */
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(.15f,.15f,.25f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        lbPontuacao.setPosition(10, camera.viewportHeight - 20);
+
+        palco.act(delta);
+        palco.draw();
+
+
+    }
+
+    /**
+     * É chamado sempre há uma alteração no tamanho da tela
+     * @param width Novo valor de largura da tela
+     * @param height Novo valor de altura da tela
+     */
+
+    @Override
+    public void resize(int width, int height) {
+     camera.setToOrtho(false, width, height);
+     camera.update();
+    }
+
+    /**
+     * É chamado sempre que o jogo for minimizado
+     */
+    @Override
+    public void pause() {
+
+    }
+
+    /**
+     * É chamado sempre quando o jogo voltar para o primeiro plano
+     */
+    @Override
+    public void resume() {
+
+    }
+
+    /**
+     * É chamado quando a tela for destruida
+     */
+    @Override
+    public void dispose() {
+
+        batch.dispose();
+        palco.dispose();
+        fonte.dispose();
+
+    }
+}
